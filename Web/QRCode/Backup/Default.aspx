7fbeb6dad7dea95b5b6e1c3e1f28cae10ba467a3
chenhuajin <chenhuajin@misrobot.com>
2019-12-10 14:05:20 +0800

    add project...

---
+++ Web/QRCode/Backup/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="主页" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="QRCode._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <table width="100%">
+    <tr>
+    <td valign="top" width="50%">
+     输入需要生成二维码内容：<br />
+     <asp:TextBox ID="TextBox1" runat="server" Height="60px" TextMode="MultiLine" 
+         Width="281px"></asp:TextBox>
+     <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="Button1" runat="server" Text="生成" onclick="Button1_Click" />
+    <br />
+        <asp:Label ID="Label1" ForeColor="Red" runat="server" Text=""></asp:Label>
+    </td>
+    
+    <td align="center">
+        <asp:Image ID="Image1" runat="server" />
+    </td>
+    </tr>
+    </table>
+    
+</asp:Content>
---
+++ Web/QRCode/Backup/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+using QuickMark;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
+using ThoughtWorks.QRCode.Codec.Util;
+using System.Drawing;
+using System.Text;
+using System.Net;
+
+namespace QRCode
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string str = TextBox1.Text;
+            string path = System.Web.HttpContext.Current.Server.MapPath("images");
+            string file = path + "\\code.png";
+            if (ToQRCode(str, file))
+            {
+                Image1.ImageUrl = "images/code.png";
+                Label1.Text = "生成成功";
+            }
+            else
+                Label1.Text = "生成失败";
+        }
+        private bool ToQRCode(string str, string filename)
+        {
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+            qrCodeEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+            qrCodeEncoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M;
+            qrCodeEncoder.QRCodeVersion = 7;
+            qrCodeEncoder.QRCodeScale = 4;
+            //生成图像
+            Bitmap image = qrCodeEncoder.Encode(str, Encoding.Default);
+            //将image保存到filename
+            image.Save(filename);
+            return true;
+        }
+    }
+}
